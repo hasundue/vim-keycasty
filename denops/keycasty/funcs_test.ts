39c6d945dc90493dee4fc72768b0a17d651d2c3e
Shun Ueda <hasundue@gmail.com>
2022-04-26 09:23:16 +0900

    refactor: refactoring of test code

--- denops/keycasty/funcs_test.ts
+++ denops/keycasty/funcs_test.ts
@@ -1,37 +1,40 @@
 import { assertEquals } from "https://deno.land/std@0.136.0/testing/asserts.ts";
 import { getWordPositions, getChunkPositions } from "./funcs.ts";
 
-Deno.test("getWordPosition 1", () => {
-  const str = "This is a test.";
-  const { starts, ends } = getWordPositions(str);
-  assertEquals(starts, [0, 5, 8, 10])
-  assertEquals(ends, [3, 6, 8, 13, 14])
+const tests = [
+  {
+    str: "This is a test.",
+    words: {
+      starts: [0, 5, 8, 10],
+      ends: [3, 6, 8, 13, 14],
+    },
+    chunks: {
+      starts: [0, 5, 8, 10],
+      ends: [3, 6, 8, 14],
+    },
+  }
+];
+
+Deno.test("getWordPosition", async (t) => {
+  for (const test of tests) {
+    const index = tests.indexOf(test);
+
+    await t.step(`case ${index + 1}: "${test.str}"`, () => {
+      const { starts, ends } = getWordPositions(test.str);
+      assertEquals(starts, test.words.starts);
+      assertEquals(ends, test.words.ends);
+    });
+  }
 });
 
-Deno.test("getWordPosition 2", () => {
-  const str = "a:b::c;";
-  const { starts, ends } = getWordPositions(str);
-  assertEquals(starts, [0, 1, 2, 3, 5, 6])
-  assertEquals(ends, [0, 1, 2, 4, 5, 6])
-});
+Deno.test("getChunkPosition", async (t) => {
+  for (const test of tests) {
+    const index = tests.indexOf(test);
 
-Deno.test("getWordPosition 3", () => {
-  const str = "# Test;";
-  const { starts, ends } = getWordPositions(str);
-  assertEquals(starts, [0, 2, 6])
-  assertEquals(ends, [0, 5, 6])
-});
-
-Deno.test("getWordPosition 4", () => {
-  const str = "# Tech Stacks";
-  const { starts, ends } = getWordPositions(str);
-  assertEquals(starts, [0, 2, 7])
-  assertEquals(ends, [0, 5, 12])
-});
-
-Deno.test("getChunkPosition", () => {
-  const str = "This is a test.";
-  const { starts, ends } = getChunkPositions(str);
-  assertEquals(starts, [0, 5, 8, 10]);
-  assertEquals(ends, [3, 6, 8, 14]);
+    await t.step(`case ${index + 1}: "${test.str}"`, () => {
+      const { starts, ends } = getChunkPositions(test.str);
+      assertEquals(starts, test.chunks.starts);
+      assertEquals(ends, test.chunks.ends);
+    });
+  }
 });
