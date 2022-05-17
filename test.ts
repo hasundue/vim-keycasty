b240a20e571dc16712552072c03ac6ac9f047326
Shun Ueda <hasundue@gmail.com>
2022-05-18 01:11:19 +0900

    test: fix the test and use the hosted version of denocy.vim

--- test.ts
+++ test.ts
@@ -1,11 +1,10 @@
-import { test } from "./denocy.vim/mod.ts";
+import { test } from "https://pax.deno.dev/hasundue/denocy.vim/mod.ts";
 
-test("test", { target: "all", delay: 50, timeout: 1000 }, (vim) => {
+test("test", { target: "all", delay: 20 }, (vim) => {
   vim.edit("./README.md");
+  vim.cmd("normal gg");
   vim.cmd("KeycastyEnable");
   vim.moveTo("keycasty");
   vim.popup.should.exist();
-  vim.popup.should.not.beEmpty();
-  vim.popup.should.include("11w");
   vim.popup.should.onlyInclude("11w");
 });
